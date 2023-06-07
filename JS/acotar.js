27516c19c4b9cc3893193b40cf78e1c056ce0fc9
gabriellegomess <128157932+gabriellegomess@users.noreply.github.com>
2023-06-07 10:02:31 -0300

    Varias att

--- JS/acotar.js
+++ JS/acotar.js
@@ -1,8 +1,10 @@
 //Se a pessoa quiser ver as imagens da pagina ela da o ok e as imagens vao aparecer
 
+alert("ALERTA DE SPOILER");
+
 let escolha = confirm("Você aceita ver todas as imagens da página?");
 
 if(escolha) {
-    document.querySelector(".acotar").style.display = "inline-block";
-  // document.querySelector(".ganso").setAttribute("class", "visivel");//atribuindo a classe 
-} 
+    document.querySelector(".acotar").style.visibility = "hidden";
+}
+
