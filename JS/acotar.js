75089ad2272ab2763944f5db59632ac40213623e
B1nha <128157932+B1nha@users.noreply.github.com>
2023-06-18 21:43:11 -0300

    Shadow

--- JS/acotar.js
+++ JS/acotar.js
@@ -1,10 +1,3 @@
 //Se a pessoa quiser ver as imagens da pagina ela da o ok e as imagens vao aparecer
 
 alert("ALERTA DE SPOILER");
-
-let escolha = confirm("Você aceita ver todas as imagens da página?");
-
-if(escolha) {
-    document.querySelector(".acotar").style.visibility = "hidden";
-}
-
